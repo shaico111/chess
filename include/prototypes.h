fc6853ab6138a7f2c4dae05d4af215c508499d9f
shaico111 <shai.co.new@gmail.com>
2024-06-21 01:13:55 +0300

    Update prototypes.h

--- include/prototypes.h
+++ include/prototypes.h
@@ -1,19 +1,18 @@
-/**
- * @file prototypes.h
- *
- * @brief Declares all functions used within the chessboard project.
- *
- * Provides a centralized declaration of functions, making it easier to understand the
- * operations related to chess game logic and movement, especially focusing on knight's
- * movements and path finding strategies.
+/* File: prototypes.h
+ * -------------------
+ * This header file includes declarations of all the functions used in the chessboard
+ * project. It provides a central place to see all operations related to chess
+ * game logic and movement.
  */
+
 #ifndef PROTOTYPES_H
 #define PROTOTYPES_H
 
 #include "typedefs.h"  // Include type definitions for function prototypes.
-#include "macros.h"    // Include macro definitions for function prototypes.
 
-// Function prototypes for all operations
+#include "macros.h"  // Include type macros for function prototypes.
+
+// Function prototypes
 void display(chessPosList *list);
 void HandleChessPosList(chessPosList *list, int** board);
 void printColumnNumbers();
